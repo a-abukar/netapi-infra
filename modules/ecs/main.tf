e966354180f86d403181c6fd32106e08adc31fde
Abdurahman Abukar <abzabukar99@gmail.com>
2024-01-15 12:07:33 +0000

    Update ecs and rds modules to include security features

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -9,6 +9,7 @@ resource "aws_ecs_task_definition" "task" {
   cpu                      = var.task_cpu
   memory                   = var.task_memory
   execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
 
   container_definitions = jsonencode([
     {
@@ -55,8 +56,30 @@ resource "aws_iam_role" "ecs_execution_role" {
   })
 }
 
-# Attach necessary policies to the role
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs_task_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+
 resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_role_policy" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskRolePolicy"
 }
--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,3 +1,8 @@
+resource "aws_kms_key" "rds_encryption" {
+  description = "KMS key for RDS encryption"
+  // Add any specific configurations for your KMS key here
+}
+
 resource "aws_db_instance" "default" {
   allocated_storage      = var.allocated_storage
   storage_type           = var.storage_type
@@ -10,8 +15,11 @@ resource "aws_db_instance" "default" {
   parameter_group_name   = aws_db_parameter_group.default.name
   db_subnet_group_name   = aws_db_subnet_group.default.name
   vpc_security_group_ids = [var.security_group_id]
-
-  skip_final_snapshot = true
+  storage_encrypted      = true
+  kms_key_id             = aws_kms_key.rds_encryption.arn
+  publicly_accessible    = false
+  skip_final_snapshot    = true
+  iam_database_authentication_enabled = true
 }
 
 resource "aws_db_subnet_group" "default" {
@@ -28,7 +36,10 @@ resource "aws_db_parameter_group" "default" {
     value = "utf8"
   }
 
-  # Additional parameters can be added here
+  parameter {
+      name = "require_ssl"
+      value = "1"
+  }
 }
 
 
